9be7cb0d0a955d8366000013cff198aa75f93b87
Kenni Woodard <kennikristine@gmail.com>
2022-11-03 15:16:02 -0700

    built out dom elements for section one

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 /* Imports */
 
 /* Get DOM Elements */
-
+const activitiesSelect = document.getElementById('activities-select');
+const activitiesImg = document.getElementById('activities-img');
+const activitiesChangeCounter = document.getElementById('activities-counter');
 /* State */
 
+let activitiesCounter = 0;
+
 /* Events */
+activitiesSelect.addEventListener('change', (e) => {
+    const value = e.target.value;
+    activitiesImg.src = `./assets/activities-${value}.jpg`;
+
+    activitiesCounter++;
+});
 
 /* Display Functions */
 
